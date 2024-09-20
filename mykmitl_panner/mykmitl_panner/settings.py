fb460ad7f2408ca4741a3106bd3d8afa2d039f41
SupakornThongaerd <supakorn642@gmail.com>
2024-09-21 00:53:42 +0700

    up static floder

--- mykmitl_panner/mykmitl_panner/settings.py
+++ mykmitl_panner/mykmitl_panner/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    "django.contrib.humanize",
+    
+    
     'tailwind',
     'theme',
     'django_browser_reload',
@@ -129,6 +132,10 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
