1e5abcf7d5226446f86fd5687ac98a8d9b5205c7
SupakornThongaerd <65070221@kmitl.ac.th>
2024-09-21 00:28:33 +0700

    update model

--- mykmitl_panner/panner/views.py
+++ mykmitl_panner/panner/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render, redirect
 from django.views import View
+from .models import *
 
 class MainView(View):
     
     def get(self, request):
+        student = Student.objects.get(pk=1)
         return render(request, "index.html",{
-            
+            "student" : student,
         })
         
         
