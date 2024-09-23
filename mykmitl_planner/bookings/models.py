7cfa7a1fc75caddd6ce991972cf47dd83defa246
SupakornThongaerd <supakorn642@gmail.com>
2024-09-23 18:31:12 +0700

    change app

--- mykmitl_planner/bookings/models.py
+++ mykmitl_planner/bookings/models.py
@@ -1,30 +1,5 @@
 from django.db import models
-
-class Facility(models.Model):
-    staff = models.ForeignKey('planner.UniversityStaff', on_delete=models.CASCADE)
-    name = models.CharField(max_length=100)
-    description = models.CharField(max_length=200)
-    opening = models.DateTimeField()
-    closing = models.DateTimeField()
-    location = models.CharField(max_length=100)
-    status = models.CharField(max_length=20)
-    capacity = models.IntegerField()
-
-    def __str__(self):
-        return self.name
-
-class Event(models.Model):
-    staff = models.ForeignKey('planner.UniversityStaff', on_delete=models.CASCADE)
-    name = models.CharField(max_length=50)
-    description = models.CharField(max_length=50)
-    start_time = models.DateTimeField()
-    end_time = models.DateTimeField()
-    status = models.CharField(max_length=20)
-    facility = models.ManyToManyField(Facility)
-    
-    def __str__(self):
-        return self.name
-    
+from planner.models import Facility    
 
 class Booking(models.Model):
     facility = models.ForeignKey(Facility, on_delete=models.CASCADE)
--- mykmitl_planner/planner/models.py
+++ mykmitl_planner/planner/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 from django.contrib.auth.models import User
-from bookings.models import Facility, Event
 
 class Student(models.Model):
     student_user = models.ForeignKey(User, on_delete=models.CASCADE)
@@ -22,6 +21,32 @@ class UniversityStaff(models.Model):
 
     def __str__(self):
         return f"{self.first_name} {self.last_name}"
+
+class Facility(models.Model):
+    staff = models.ForeignKey('planner.UniversityStaff', on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    description = models.CharField(max_length=200)
+    opening = models.DateTimeField()
+    closing = models.DateTimeField()
+    location = models.CharField(max_length=100)
+    status = models.CharField(max_length=20)
+    capacity = models.IntegerField()
+
+    def __str__(self):
+        return self.name
+
+class Event(models.Model):
+    staff = models.ForeignKey('planner.UniversityStaff', on_delete=models.CASCADE)
+    name = models.CharField(max_length=50)
+    description = models.CharField(max_length=50)
+    start_time = models.DateTimeField()
+    end_time = models.DateTimeField()
+    status = models.CharField(max_length=20)
+    facility = models.ManyToManyField(Facility)
+    
+    def __str__(self):
+        return self.name
+    
 class Schedule(models.Model):
     student = models.ForeignKey(Student, on_delete=models.CASCADE)
     facility = models.ForeignKey(Facility, on_delete=models.SET_NULL, null=True, blank=True)
@@ -33,3 +58,4 @@ class Schedule(models.Model):
 
     def __str__(self):
         return self.title
+
