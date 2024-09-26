4084a9d6c0098e0b3244cc6bcf7846915540284e
65070188Mananchaya <65070188@kmitl.ac.th>
2024-09-27 00:45:49 +0700

    update

--- mykmitl_planner/bookings/urls.py
+++ mykmitl_planner/bookings/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-
+    path("", views.RecommendListPage.as_view(), name="recommend-list"),
      
 ]
--- mykmitl_planner/bookings/views.py
+++ mykmitl_planner/bookings/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.views import View
+from .models import *
 
 # Create your views here.
+class RecommendListPage(View):
+    
+    def get(self, request):
+        return render(request, "recommend-list.html",{
+
+        })
